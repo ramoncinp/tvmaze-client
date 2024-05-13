38991acceaed78c0397f3528a794deb5573f9f82
aldairparra <aldairparrag@gmail.com>
2024-05-12 22:14:10 -0600

    Remove glide dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,10 +73,6 @@ dependencies {
     implementation ("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation ("androidx.navigation:navigation-ui-ktx:2.7.7")
 
-    // Glide
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
-
     // Picasso
     implementation("com.squareup.picasso:picasso:2.8")
 
